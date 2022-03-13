71dbbf20bab99f508abf82776c2bdfe9f31f8859
Abhisek Joshi <nofursad@gmail.com>
2022-03-13 21:33:47 +0000

    Load whole page for test

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,16 +1,23 @@
 /**
  * @jest-environment jsdom
  */
-const { TestWatcher } = require("jest");
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>"
+    // document.body.innerHTML = "<p id='par'></p>"
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM test", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
-    })
-})
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
+});
